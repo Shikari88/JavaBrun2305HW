76cae324e7ee144dc826ae7218da312e95509af8
Shikari88 <astakhin.rsm@mail.ru>
2022-06-09 12:49:31 +0300

    Add tasks 4

---
+++ JavaOnly/src/Lesson4/HW/PhoneNumber.java
@@ -0,0 +1,77 @@
+package Lesson4.HW;
+
+public class PhoneNumber {
+    /*
+    Создать класс PhoneNumber (в отдельном файле)
+    2 поля:
+    num целочисленного типа
+        (там будет храниться номер в виде числа, начинающегося с 8)
+    numType строчного типа
+        (там будет храниться информация о типе номера, например "Рабочий" или "Личный")
+    В psvm создать объект класса PhoneNumber, задать его полям значения через операцию присваивания
+    Вывести информацию об объекте в консоль в одну строку
+     */
+
+    //HW1
+    long num;
+    String numType;
+    //
+
+    //HW2
+    public PhoneNumber(){
+        this.numType = "";
+    }
+
+    public PhoneNumber(long num, String numType){
+        this.num = num;
+        this.numType = numType;
+    }
+    //
+
+    //HW3
+    void print(){
+        System.out.println(this);
+    }
+
+    void changeType(String numType){
+        this.numType = numType;
+    }
+
+    long getNum(){
+        return num;
+    }
+
+    @Override
+    public String toString(){
+        return "Номер абонента " + num + "; тип номера: " + numType;
+    }
+    public static void main(String[] args) {
+        //HW1
+        PhoneNumber phoneNumber = new PhoneNumber();
+        phoneNumber.num = 8_965_666_33_22L;
+        phoneNumber.numType = "Рабочий";
+        System.out.println("Номер абонента " + phoneNumber.num + "; тип номера: " + phoneNumber.numType);
+        System.out.println(); //для разделения вывода результатов задач
+        //
+
+        //HW2
+        PhoneNumber phoneNumber1 = new PhoneNumber();
+        PhoneNumber phoneNumber2 = new PhoneNumber(8_981_555_33_77L, "Домашний");
+
+        phoneNumber1.num = 8_999_666_88_44L;
+        phoneNumber1.numType = "Личный";
+
+        System.out.println(phoneNumber1);
+        System.out.println(phoneNumber2);
+        System.out.println();//для разделения вывода результатов задач
+
+        //HW3
+
+        phoneNumber2.print();
+
+        System.out.println(phoneNumber2.getNum());
+
+        phoneNumber2.changeType("Пейджер");
+        System.out.println(phoneNumber2);
+    }
+}
